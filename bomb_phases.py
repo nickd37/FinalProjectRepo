be5ade2b2eaab3320127e05ff8084805e31a58cd
nickd37 <dobrzyckinick@gmail.com>
2025-05-06 22:58:16 -0400

    Update bomb_phases.py

--- bomb_phases.py
+++ bomb_phases.py
@@ -1,7 +1,7 @@
 #################################
 # CSC 102 Defuse the Bomb Project
 # GUI and Phase class definitions
-# Team: 
+# Team: Nicholas Dobrzycki, Vincent DePalo, Jay Briskin
 #################################
 
 # import the configs
